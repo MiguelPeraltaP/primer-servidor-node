ba940c2273965dcf28689716539b740754e16312
Miguel <a00822952@itesm.mx>
2022-05-31 20:35:56 -0500

    resized images

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
 //Obtener una funcion que se exporta del paquete express
 const express = require('express');
-//Asignar lo que arroje la funcion express a la variable app
-const app = express();
+//Dependencia para recibir imagenes
+const multer = require('multer');
+//Dependencia para modificar imagenes
+const sharp = require('sharp');
+//Modulo para interactuar con el file system
+const fs = require('fs');
 
-//req: expresion que se usa para obtener los datos de la peticion que va a llegar a nuestro servidor
-//res: expresion que se usa para enviar los datos que se van a enviar al cliente que solicito la peticion
+//Guarda la imagen en el directorio uploads
+//const upload = multer({dest: './uploads'});
+//Guarda la imagen en memoria
+const sotrageStrategy = multer.memoryStorage()
+const upload = multer({storage: sotrageStrategy})
+
+//Asignar lo que arroje la funcion express a la variable app
+const app = express()
+
+//Returns middleware that only parses json and only looks at requests where
+//the Content-Type header matches the type option
+app.use(express.json())
+
+/*
+Get: obtener datos del navegador
+req: expresion que se usa para obtener los datos de la peticion que va a llegar a nuestro servidor
+res: expresion que se usa para enviar los datos que se van a enviar al cliente que solicito la peticion
+*/
 app.get('/', (req, res) => {
-    res.send('Hola mundo');
+    res.send('Hola mundo')
+})
+
+/*
+Post: enviar datos al servidor
+*/
+app.post('/imagen', upload.single('imagen'), async(req, res) => {
+    //Obtener el archivo que se va a enviar
+    //const body = req.body
+    //console.log(body)
+
+    const imagen = req.file
+    //console.log(imagen)
+
+    //Obtener el buffer de la imagen
+    const processedImage = sharp(imagen.buffer)
+    //Remasterizacion de la imagen
+    const rezisedImage = processedImage.resize(800, 200, {
+        fit: "contain",
+        background: "#FFF"
+    })
+    //Buffer de datos de nuestra imagen remasterizada
+    const finalImage = await rezisedImage.toBuffer()
+
+    fs.writeFileSync('images/graduacion.png', finalImage)
+
+    console.log(rezisedImage)
+
+    res.send({resizedImage: finalImage})
 })
 
 //Servidor escucha en el puerto 3000 (localhost:3000)
-app.listen(3000);
+app.listen(3000)
